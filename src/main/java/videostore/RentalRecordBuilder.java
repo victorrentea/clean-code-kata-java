99b87a0409880eec2e3a3dd9f18965e4af5f85ed
Marius Ion <marius.ion@gameloft.com>
2020-06-04 12:55:39 +0300

    Rename totalPrice

--- src/main/java/videostore/RentalRecordBuilder.java
+++ src/main/java/videostore/RentalRecordBuilder.java
@@ -10,25 +10,25 @@ public class RentalRecordBuilder {
     }
 
     public String build() {
-        double totalAmount = 0;
+        double totalPrice = 0;
         int frequentRenterPoints = 0;
 
         String result = headerLines();
         for (Rental rental: customer.getRentals()) {
             frequentRenterPoints += rental.getPoints();
             result += "\t" + rental.getMovie().getTitle() + "\t" + rental.getPrice() + "\n";
-            totalAmount += rental.getPrice();
+            totalPrice += rental.getPrice();
         }
 
-        return result + footerLines(totalAmount, frequentRenterPoints);
+        return result + footerLines(totalPrice, frequentRenterPoints);
     }
 
     private String headerLines() {
         return "Rental Record for " + customer.getName() + "\n";
     }
 
-    private String footerLines(double totalAmount, int frequentRenterPoints) {
-        String result = "Amount owed is " + totalAmount + "\n";
+    private String footerLines(double totalPrice, int frequentRenterPoints) {
+        String result = "Amount owed is " + totalPrice + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints)
                 + " frequent renter points";
         return result;
