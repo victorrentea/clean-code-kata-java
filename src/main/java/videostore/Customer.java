6c288d83d0b7e8fe7c98a50e113dc22859ccc0da
thingmarius <marius.ion85@gmail.com>
2020-06-04 04:26:25 -0400

    Add StatementBuilder class

--- src/main/java/videostore/Customer.java
+++ src/main/java/videostore/Customer.java
@@ -18,49 +18,8 @@ public class Customer {
         return name;
     }
 
-   public String addFooterLines(double totalAmount, int frequentRenterPoints) {
-      String result = "Amount owed is " + totalAmount + "\n";
-      result += "You earned " + String.valueOf(frequentRenterPoints)
-              + " frequent renter points";
-      return result;
-   }
-
    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Iterator<Rental> rentals = this.rentals.iterator();
-        String result = "Rental Record for " + getName() + "\n";
-        while (rentals.hasNext()) {
-            double thisAmount = 0;
-            Rental each = rentals.next();
-            // determine amounts for each line
-            switch (each.getMovie().getCategory()) {
-                case REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getCategory() == Movie.Category.NEW_RELEASE)
-                    && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t"
-                    + thisAmount + "\n";
-            totalAmount += thisAmount;
-        }
-      return addFooterLines(totalAmount, frequentRenterPoints);
+        StatementBuilder builder = new StatementBuilder(name, rentals);
+        return builder.build();
     }
 }
---
+++ src/main/java/videostore/StatementBuilder.java
@@ -0,0 +1,63 @@
+package videostore;
+
+import java.util.*;
+
+public class StatementBuilder {
+
+    private final String name;
+    private final List<Rental> rentals;
+
+    public StatementBuilder(String name, List<Rental> rentals) {
+        //TODO add validations?
+        this.name = name;
+        this.rentals = rentals;
+    }
+
+    public String build() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Iterator<Rental> rentals = this.rentals.iterator();
+        String result = "Rental Record for " + name + "\n";
+        while (rentals.hasNext()) {
+            double thisAmount = 0;
+            Rental each = rentals.next();
+            // determine amounts for each line
+            switch (each.getMovie().getCategory()) {
+                case REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case CHILDREN:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getCategory() == Movie.Category.NEW_RELEASE)
+                    && each.getDaysRented() > 1)
+                frequentRenterPoints++;
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t"
+                    + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+      return result + addFooterLines(totalAmount, frequentRenterPoints);
+    }
+
+   private String addFooterLines(double totalAmount, int frequentRenterPoints) {
+      String result = "Amount owed is " + totalAmount + "\n";
+      result += "You earned " + String.valueOf(frequentRenterPoints)
+              + " frequent renter points";
+      return result;
+   }
+
+}
+
